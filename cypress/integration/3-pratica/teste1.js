9d3c0abc50a3943ae8868e4365e21cb8a7cb40fb
unknown <d.assis.arte@gmail.com>
2022-04-12 21:50:40 -0300

    subindo prática com aplicação local

--- cypress/integration/3-pratica/teste1.js
+++ cypress/integration/3-pratica/teste1.js
@@ -1,23 +1,42 @@
 describe('primeiros testes',()=>{
-    beforeEach(()=>{
-        //visitar home do sistema
-        cy.visit('http://automationpractice.com/index.php');
+    beforeEach('visitar site',() => {
+        cy.visit('signin');
     });
 
-    it('visitar página',()=>{        
+    it('checar página', ()=>{        
         // verificar existência de elemento único da página
         // "cy.get('.logo').should('exist')" é uma expressão redundante, uma vez que alguns comandos de api do cypress já possuem asserts embutidos, como é o caso de cy.get()
         // Sendo assim, caso o alvo de cy.get() não exista na interface, o teste falhará
-        cy.get('.logo')
+        cy.get('.makeStyles-logo-3')
+        cy.url().then((url) => expect(url).to.include("signin"))
     });
 
-    it('navegar para T-shirts',()=>{
-        //clicar no botão de redirect
-        cy.get('.sf-menu > :nth-child(3) > a').click();
-        //verificar url
-        cy.url().should('equal','http://automationpractice.com/index.php?id_category=5&controller=category')
+    it('login inválido (credenciais inválidas)',()=>{
+       cy.get("#username").type("abcd");
+       cy.get("#password").type("1234");
+       cy.get('button[data-test="signin-submit"]').click()
+       cy.contains("invalid")
+    });
 
-        cy.get()
-    })
-    
+    it('login inválido (campos vazios)',()=>{
+        cy.get('button[data-test="signin-submit"]').click()
+        cy.contains("required")
+     });
+
+    it('login inválido (user vazio)',() => {
+        cy.get("#password").type("1234");
+        cy.contains("required")
+        cy.get('button[data-test="signin-submit"]').should("be.disabled")
+     });
+
+    it('login inválido (senha vazia)',() => {
+        cy.get("#username").type("abcd");
+        cy.get('button[data-test="signin-submit"]').should("be.disabled")
+     });
+
+    it('login válido',()=>{
+        cy.get("#username").type("Giovanna74");
+        cy.get("#password").type("$2a$10$5PXHGtcsckWtAprT5/JmluhR13f16BL8SIGhvAKNP.Dhxkt69FfzW");
+        cy.get('button[data-test="signin-submit"]').click()
+     });
 })
