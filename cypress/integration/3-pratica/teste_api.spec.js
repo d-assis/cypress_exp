8d2eabd54af992857a4c8a281614fcac06135e87
unknown <d.assis.arte@gmail.com>
2022-04-22 17:46:50 -0300

    experimentando sobreescrições com git

--- cypress/integration/3-pratica/teste_api.spec.js
+++ cypress/integration/3-pratica/teste_api.spec.js
@@ -1,16 +1,29 @@
 import * as api from "../endpoints/endpoints.js"
 
 describe("testes de API",() =>{
-    it('ciclo de usuário',()=> {
-        api.endpoints.signUp().then((response) => {
-            expect(response.status).to.eql(201);
-            cy.log(response.body)
-            api.endpoints.login(response.body.user.username).its('status').should('eql',200);
-            api.endpoints.updateUser(response.body.user.id).its('status').should('eql',204);
-            api.endpoints.chargeMoney(response.body.user.id).its('status').should('eql',200);
-            api.endpoints.checkTransactions().then((r) => {
-                expect(r.status).to.eql(200)
-            })
-        })
+    before('criar usuário', () => {
+           api.endpoints.signUp().as('u')
     });
+    
+   it('verifica usuário criado',() => {
+        
+        cy.get('@u').then((response) => {
+           cy.log(response.body)
+           expect(response.status).to.eql(201)
+       })
+   });
+
+   it('trololo', () => {
+       cy.get('@u').then((response) => {
+           cy.log(response.body.user.id)
+       })
+   })
+   
+   /*
+   it('login com usuário criado',() => {
+        cy.get('@u').then((response) => {
+            api.endpoints.login(response.body.user.username).its('status').should('eql',200)
+        })
+   })
+   */
 })
