5e6156ad7596d7013b3cf395a9feb8937c63fc37
unknown <d.assis.arte@gmail.com>
2022-04-25 06:47:23 -0300

    subindo ajuste da bateria de testes de API

--- cypress/integration/3-pratica/teste_api.spec.js
+++ cypress/integration/3-pratica/teste_api.spec.js
@@ -1,29 +1,45 @@
 import * as api from "../endpoints/endpoints.js"
 
 describe("testes de API",() =>{
-    before('criar usuário', () => {
+    beforeEach('criar usuário', () => {
            api.endpoints.signUp().as('u')
     });
     
-   it('verifica usuário criado',() => {
-        
+   it('verifica status code',() => {
         cy.get('@u').then((response) => {
            cy.log(response.body)
            expect(response.status).to.eql(201)
        })
    });
 
-   it('trololo', () => {
-       cy.get('@u').then((response) => {
-           cy.log(response.body.user.id)
-       })
-   })
-   
-   /*
-   it('login com usuário criado',() => {
+   it('login de usuário', () => {
         cy.get('@u').then((response) => {
+            cy.log(response.body.user.id);
             api.endpoints.login(response.body.user.username).its('status').should('eql',200)
         })
-   })
-   */
+    });
+
+    it('alterar informações de usuário', () => {
+        cy.get('@u').then((response) => {
+            cy.log(response.body.user.id);
+            api.endpoints.login(response.body.user.username);
+            api.endpoints.updateUser(response.body.user.id).its('status').should('eql',204)
+        })
+    });
+
+    it('cobrança de valor', () => {
+        cy.get('@u').then((response) => {
+            cy.log(response.body.user.id);
+            api.endpoints.login(response.body.user.username);
+            api.endpoints.chargeMoney(response.body.user.id).its('status').should('eql',200)
+        })
+    });
+
+    it('verificar transações', () => {
+        cy.get('@u').then((response) => {
+            cy.log(response.body.user.id);
+            api.endpoints.login(response.body.user.username);
+            api.endpoints.checkTransactions().its('status').should('eql',200)
+        })
+    });
 })
